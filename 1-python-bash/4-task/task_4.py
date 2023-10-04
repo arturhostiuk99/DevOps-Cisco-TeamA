bb192281bdf31b71cb28b3a2430ff66ef7d23460
MaksymukNatalia <146457298+MaksymukNatalia@users.noreply.github.com>
2023-10-04 20:21:52 +0300

    fix file check function and add list of files value task_4.py

--- 1-python-bash/4-task/task_4.py
+++ 1-python-bash/4-task/task_4.py
@@ -9,23 +9,26 @@ parser.add_argument('-s', '--string', help='The string you need to find')
 args = parser.parse_args()
 
 
-def check_parse_args(path, string):
+def availability_parse_args(path, string):
     """This function checks if arguments were provided when running the python script"""
-    if not path:
-        print(f'You need to specify the path')
+    if not path and not string:
+        print(f'You need to specify the path and a string to search for')
         exit()
-    if not string:
+    elif not string:
         print(f'You need to specify a string to search for')
         exit()
+    elif not path:
+        print(f'You need to specify the path')
+        exit()
 
-
-check_parse_args(args.path, args.string)
+availability_parse_args(args.path, args.string)
 print("Search...")
 
 
 def return_files(preset_path, search_string):
     """This function searches the directories that are in the path,
     searches all files and checks whether the given line is present in the file"""
+    files = []
     if os.path.isdir(preset_path):
         for dir_path, dir_names, filenames in os.walk(preset_path):
             for filename in filenames:
@@ -33,18 +36,17 @@ def return_files(preset_path, search_string):
                 try:
                     with open(file_path, 'r') as f:
                         if search_string in f.read():
-                            print(f"string '{search_string}' was found in the path {file_path}")
+                              files.append(file_path)
                 except Exception as e:
-                    print(f'Error reading file {file_path}: {str(e)}')
+                    return f'Error reading file {file_path}: {str(e)}'
     elif os.path.isfile(preset_path):
         try:
             with open(preset_path, 'r') as f:
                 if search_string in f.read():
-                    print(f"string '{search_string}' was found in the path {preset_path}")
+                   return preset_path
         except Exception as e:
-            print(f'Error reading file {preset_path}: {str(e)}')
-    else:
-        print('Check the path. Maybe it does not exist or the specified one is incorrect')
+            return f'Error reading file {preset_path}: {str(e)}'
+    return print(files)
 
 
 return_files(args.path, args.string)
