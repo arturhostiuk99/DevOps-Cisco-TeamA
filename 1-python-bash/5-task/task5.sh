843c61cb24b2ecc26126aec1404758d9062b8349
Iaroslav Koval <146448189+IaKoval@users.noreply.github.com>
2023-10-04 20:55:06 +0300

    Update task5.sh
    I removed the comments so as not to load the code

--- 1-python-bash/5-task/task5.sh
+++ 1-python-bash/5-task/task5.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Check the number of passed arguments
 if [ "$#" -ne 2 ]; then
 	echo "Використання: $0 <шлях_до_директорії> <розширення_файлів>"
 	exit 1
 fi
 
-# Get arguments
 directory="$1"
 extension="$2"
 
-# Check whether the directory exists
 if [ ! -d "$directory" ]; then
 	echo "Директорія '$directory' не існує."
 	exit 1
 fi
 
-# Find files with the specified extension and output them
 find "$directory" -type f -name "*.$extension" -print
 
