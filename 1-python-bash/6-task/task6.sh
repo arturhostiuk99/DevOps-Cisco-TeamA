ff8e5c5c04b73d08756e06dc30e07de5d534997b
Iaroslav Koval <146448189+IaKoval@users.noreply.github.com>
2023-10-04 20:56:51 +0300

    Update task6.sh
    I removed the comments so as not to load the code and add "$directory"

--- 1-python-bash/6-task/task6.sh
+++ 1-python-bash/6-task/task6.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Check the number of passed arguments
  if [ "$#" -ne 2 ]; then
     echo "Використання: $0 <шлях_до_директорії> <текст_для_пошуку>"
     exit 1
 fi
 
-# Get arguments
 directory="$1"
 search_text="$2"
 
-# Check whether the directory exists
 if [ ! -d "$directory" ]; then
     echo "Директорія '$directory' не існує."
     exit 1
 fi
 
-# Find files that contain the specified text
 found_files=$(grep -rl "$search_text" "$directory")
 
 if [ -z "$found_files" ]; then
