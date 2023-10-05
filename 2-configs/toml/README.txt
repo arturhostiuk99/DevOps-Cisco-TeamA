49c896189ff72734a28ca51453c4d559830b5f9a
Dimus <dislakoval@gmail.com>
2023-10-05 14:00:01 +0300

    Read toml file python script

---
+++ 2-configs/toml/README.txt
@@ -0,0 +1,2 @@
+File with extension ".toml" must be in the same directory as the script.
+Script search all passwords. 
---
+++ 2-configs/toml/toml-task.py
@@ -0,0 +1,33 @@
+import tomllib
+import os
+
+FILE_NAME = 'test.toml'
+
+
+def file_exist():
+    path = os.path.join(os.getcwd(), FILE_NAME)
+    if os.path.exists(path):
+        return True
+
+
+def read_toml_file(file):
+    try:
+        with open(file, "rb") as f:
+            data = tomllib.load(f)
+        return data
+    except Exception as e:
+        print("Can't read a file \n{}".format(e))
+        return breakpoint()
+
+
+def main():
+    if file_exist():
+        text = read_toml_file(FILE_NAME)
+        for info, user in text.items():
+            for key, value in user.items():
+                if key == 'password':
+                    print(value)
+
+
+if __name__ == '__main__':
+    main()
