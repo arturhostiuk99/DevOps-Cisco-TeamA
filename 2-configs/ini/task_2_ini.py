6189830ece696ce793bc803820bf056b91d716d0
MaksymukNatalia <146457298+MaksymukNatalia@users.noreply.github.com>
2023-10-05 20:29:10 +0300

    Add validation task_2_ini.py

--- 2-configs/ini/task_2_ini.py
+++ 2-configs/ini/task_2_ini.py
@@ -1,8 +1,29 @@
 import configparser
+import os
 
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-password_value = config.get("keys", "password")
+path_to_ini = 'config.ini'
+search_key = 'password'
 
-print(password_value)
+
+def exist_ini(exists_ini):
+    if os.path.exists(exists_ini):
+        search_pass(exists_ini, search_key)
+    else:
+        return print(f'file .ini doesnt not exists')
+
+
+def search_pass(file_ini, param):
+    config = configparser.ConfigParser()
+    config.read(file_ini)
+    sections = config.sections()
+    passwords = []
+    for section in sections:
+        for key in config[section]:
+            if key == param:
+                passwords.append(config.get(section, key))
+    return print(passwords)
+
+
+exist_ini(path_to_ini)
+
